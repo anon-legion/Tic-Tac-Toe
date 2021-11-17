ba31b6ea568dfbd42ddb9c7d3a649f14cd9d743a
anon-legion <g.sync.mobile1@gmail.com>
2021-11-17 22:00:20 +0800

    update App.css and index.css to fit app in 100vh

--- src/App.js
+++ src/App.js
@@ -9,40 +9,40 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="hero is-fullheight is-black no-scroll">
+      <div className="hero is-black">
         <header className="hero-head">
           <div className="container has-text-centered">
             <h1 className="title is-1 has-text-primary">Tic Tac Toe</h1>
-            <h2 className="subtitle is-3">Minimax algorithm</h2>
+            <h2 className="subtitle is-4">Minimax algorithm</h2>
           </div>
         </header>
-        <main className="hero-body is-flex is-flex-direction-column is-justify-content-center mobile-LR-nopadding">
-          <GameProvider>
-            <div className="container">
-              <div className="section p-0 pb-3">
-                <GameDisplay />
-              </div>
-              <div className="grid-container">
-                <Grid />
-              </div>
-              <div className="buttons is-centered p-6">
-                <GameButtons />
-              </div>
-            </div>
-          </GameProvider>
-          <a href="https://github.com/anon-legion" target="_blank">
-            <footer className="hero-foot is-clickable has-text-centered">
-              <p className="is-size-5 has-text-grey-dark">Copyright © 2021 =GV=
-                <span className="icon-text">
-                  <span className="icon is-medium has-text-grey-dark">
-                    <i className="fab fa-github is-size-4"></i>
-                  </span>
-                </span>
-              </p>
-            </footer>
-          </a>
-        </main>
       </div>
+      <main className="is-flex is-flex-direction-column is-justify-content-center mobile-LR-nopadding">
+        <GameProvider>
+          <div className="container">
+            <div className="section p-0 pb-4 pt-5">
+              <GameDisplay />
+            </div>
+            <div className="grid-container">
+              <Grid />
+            </div>
+            <div className="buttons is-centered p-6">
+              <GameButtons />
+            </div>
+          </div>
+        </GameProvider>
+      </main>
+      <footer className="has-text-centered">
+        <a href="https://github.com/anon-legion" target="_blank">
+          <p className="is-clickable is-size-5 has-text-grey-dark">Copyright © 2021 =GV=
+            <span className="icon-text">
+              <span className="icon is-medium has-text-grey-dark">
+                <i className="fab fa-github is-size-4"></i>
+              </span>
+            </span>
+          </p>
+        </a>
+      </footer>
     </>
   );
 }
